a1351bd0b7fa2f7093a0dc791d59e9931fccecb5
Jiri Peinlich <jiri.peinlich@gmail.com>
2014-07-23 12:47:14 +0200

    fixing code

--- src/main/java/programming/exercise/AccessControlCoordinator.java
+++ src/main/java/programming/exercise/AccessControlCoordinator.java
@@ -1,25 +1,25 @@
 package programming.exercise;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class AccessControlCoordinator {
 
     AccessControlDecisionMaker accessControlDecisionMaker1;
     AccessControlDecisionMaker accessControlDecisionMaker2;
 
-    public AccessControlCoordinator(final AccessControlDecisionMaker accessControlDecisionMaker1,
-            final AccessControlDecisionMaker accessControlDecisionMaker2) {
-    }
-
     public synchronized boolean performAccessCheckForBook(final String book) {
-        if (this.accessControlDecisionMaker1.performAccessCheck("book")) {
+        checkNotNull(book);
+        if (this.accessControlDecisionMaker1.performAccessCheck(book)) {
             return true;
         }
-        if (this.accessControlDecisionMaker2.performAccessCheck(book) && !this.accessControlDecisionMaker1.performAccessCheck("book")) {
-            return false;
-        }
-        if (!this.accessControlDecisionMaker1.performAccessCheck(book) && !this.accessControlDecisionMaker2.performAccessCheck(book)) {
-            return true;
-        }
-        return true;
+        return this.accessControlDecisionMaker2.performAccessCheck(book);
     }
 
+    public void setAccessControlDecisionMaker1(AccessControlDecisionMaker accessControlDecisionMaker1) {
+        this.accessControlDecisionMaker1 = accessControlDecisionMaker1;
+    }
+
+    public void setAccessControlDecisionMaker2(AccessControlDecisionMaker accessControlDecisionMaker2) {
+        this.accessControlDecisionMaker2 = accessControlDecisionMaker2;
+    }
 }
